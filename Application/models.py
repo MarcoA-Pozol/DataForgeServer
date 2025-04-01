b78e347b071ca5800f2f3a40b3e122b2339d63db
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-03-31 22:57:12 -0600

    Added: Tier model was included with the allowed rates for each tier the user haves.

--- Application/models.py
+++ Application/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from Authentication.models import User
 
 FILE_EXTENSION_CHOICES = ['csv', 'xls', 'xlsx', 'txt', 'pdf', 'json']
+TIERS = ['Basic', 'Professional'] 
+ALLOWED_FILES_TOTAL_SIZE = [1000, 100000] # MB (1G for basic tier, 100G for professional tier)
+ALLOWED_DATABASE_QUERIES_PER_DAY = [50, 500] # Queries to the database
+ALLOWED_API_REQUESTS_PER_DAY = [20, 300]
 
 class UserFiles(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_files') # User is the foreign key the file belongs to
@@ -15,3 +19,15 @@ class UserFiles(models.Model):
         
     def __str__(self):
         return f'{self.user.username} - {self.filename}'
+        
+class Tier(models.Model):
+    name = models.CharField(max_length=20, choices=TIERS, null=False, default='Basic')
+    allowed_files_total_size = models.IntegerField(choices=ALLOWED_FILES_TOTAL_SIZE, null=False, default=ALLOWED_FILES_TOTAL_SIZE[0])
+    allowed_database_queries_per_day = models.IntegerField(choices=ALLOWED_DATABASE_QUERIES_PER_DAY, null=False, default=ALLOWED_DATABASE_QUERIES_PER_DAY[0])
+    allowed_api_request_per_day = models.IntegerField(choices=ALLOWED_API_REQUESTS_PER_DAY, null=False, default=ALLOWED_API_REQUESTS_PER_DAY[0])
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
