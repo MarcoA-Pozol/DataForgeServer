c2672970e8af35e9c122a59223b7811a457e161f
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-01 22:34:49 -0600

    Updated: Removed the not anymore neccesary classic but old authentication and registering process in order to keep the API View based authentication, session and registering management classes.

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render, redirect
-from django.utils import translation
-from . forms import RegisterForm, LoginForm
-from django.contrib import auth, messages
 # Language obtention
 from . datasets import COUNTRY_TO_LANGUAGE
 # Threading
@@ -57,59 +53,3 @@ class RegisterUserAPIView(APIView):
             return Response({'message':f'User account was successfully created: {username}'}, status=status.HTTP_201_CREATED)
         except Exception as e:
             return Response({'error':f'An error ocurred during creating a new user account: {e}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-def authentication(request):
-    """
-        Displays the registration formulary or login formulry, receives the data and creates an user account or login with the provided credentials.
-    """
-    if request.user.is_authenticated:
-        return redirect('app-home')
-    user_language = request.session.get('django_language', None)
-    if user_language:
-        translation.activate(user_language)
-        request.LANGUAGE_CODE = translation.get_language()
-
-    if request.method == 'POST':
-        register_form = RegisterForm(request.POST, request.FILES)
-        login_form = LoginForm(request.POST)
-        if register_form.is_valid():
-            user = register_form.save(commit=True)
-
-            mail.send_mail(
-                subject=f"DataForge - Created accou",
-                message=f"DataForge is the site where you will find many features for data management, like charts and reports generation, import and export in different formats and much more.",
-                from_email=EMAIL_HOST_USER, 
-                recipient_list=[user.email],
-                fail_silently=False,  # Raise an exception if email sending fails (False for debugging, True for production)
-            )
-
-            auth.login(request, user)
-            translation.activate(user.language)
-            request.session['django_language'] = user.language
-
-            if user.is_authenticated:
-                return redirect('app-home')
-            else:
-                return redirect('authentication')
-        if login_form.is_valid():
-            username = login_form.cleaned_data['username']
-            password = login_form.cleaned_data['password']
-            
-            user = auth.authenticate(request, username=username, password=password) # Check if provided credentials match with an existing user account.
-            if user is not None:
-                auth.login(request, user)
-                return redirect('app-home')
-            else:
-                messages.error(request, 'Invalid username or password.')
-    else:
-        register_form = RegisterForm()
-        login_form = LoginForm()
-    context = {'register_form':register_form, 'login_form':login_form}
-    return render(request, 'formularies/authentication.html', context)
-        
-def logout(request):
-    """
-        Close current activaly user´s session.
-    """
-    auth.logout(request)
-    return redirect('authentication')
