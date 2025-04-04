2ed2c5140d655a171e160061ef25c03d1560986b
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-04 15:18:06 -0600

    Removed: Views and urls in application app because of not neccesary template-based views.

--- Application/urls.py
+++ Application/urls.py
@@ -2,8 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.app_home, name='app-home'),
-    path('import_export_data/', views.import_export_data, name='import-export-data'),
-    path('data_visualization/', views.data_visualization, name='data-visualization'),
-    path('user_data_view/', views.user_data_view, name='user-data-view'),
 ]
